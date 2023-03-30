5c3b56144587fde0cdd94e7cfa997da5c1f23fcf
mymilind <milind.joshi@mindstix.com>
2023-03-30 17:36:25 +0530

    First commit.

---
+++ MultibranchJob/MultibranchJob/MultibranchJobApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultibranchJobApp.swift
+//  MultibranchJob
+//
+//  Created by Milind Joshi on 30/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MultibranchJobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
